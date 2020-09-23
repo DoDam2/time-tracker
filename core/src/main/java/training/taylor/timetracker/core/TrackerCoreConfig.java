258a38c1743e1058ed83433ad39ffb4b8fe63c30
DoDam2 <70992700+DoDam2@users.noreply.github.com>
2020-09-23 15:06:17 +0900

    Update TrackerCoreConfig.java

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -15,6 +15,7 @@ import java.util.List;
 @ComponentScan("training.taylor.timetracker.core")
 public class TrackerCoreConfig {
 
+    /** get */
     @Bean(name = "timesheet")
     public List<TimeEntry> timeEntries() {
         return new ArrayList<>();
